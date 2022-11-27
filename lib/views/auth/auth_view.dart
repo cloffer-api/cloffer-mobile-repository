9fc549a9a1b011444366ab56f0ffd895a2d88e9e
mgolubev1 <m.golubev1@g.nsu.ru>
2022-11-27 12:42:26 +0700

    Bloac Auth

---
+++ lib/views/auth/auth_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class AuthView extends StatefulWidget {
+  const AuthView({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _AuthViewState();
+}
+
+class _AuthViewState extends State<AuthView> {
+  final TextEditingController _emailController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
